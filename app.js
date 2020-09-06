7243755ddd518ca2c2d3c7dba266e340e0ec78fa
abhinitro <you@example.com>
2020-09-06 12:33:33 +0530

    added

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const path = require('path');
-// const app = express();
-// const port =  4004
-
-// app.use(express.static(path.join(__dirname, 'out')));
-// console.log(__dirname);
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'out', 'index.html'));
-// });
-
-// app.listen(port,console.log("port running on",port));
-
-const { createServer } = require('http')
+const express = require('express')
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 4004
@@ -25,18 +7,21 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://w.w')
-    const { pathname, query } = parsedUrl
+  const server = express()
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port, (err) => {
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query)
+  })
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
